a696ee1322dcb6fed2cd9ba12ae1f885e08d1b47
DanCher17 <dancher072@gmail.com>
2017-06-01 18:14:51 +0300

    add btnBrowse_Click

--- Form1.cs
+++ Form1.cs
@@ -9,5 +9,14 @@ namespace PictureToASCII {
         public Form1() {
             InitializeComponent();
         }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult diag = openFileDialog1.ShowDialog();
+            if (diag == DialogResult.OK)
+            {
+                txtPath.Text = openFileDialog1.FileName;
+            }
+        }
     }
 }
